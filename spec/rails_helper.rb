203aa1af599ac3219c6aff654c3a27feabb69537
Jameson Knoll <j.knoll621@gmail.com>
2019-05-13 17:29:38 -0400

    basic tests for static pages to ensure test framework is running

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-include Warden::Test::Helpers
-Warden.test_mode!
-
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
